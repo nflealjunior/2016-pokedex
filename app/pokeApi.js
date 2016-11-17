0db359e15b49962f7e2e3b2921a98c85dc026636
Edy Segura <edysegura@gmail.com>
2016-11-17 21:44:03 -0200

    Implemented catch pokemon by id

--- app/pokeApi.js
+++ app/pokeApi.js
@@ -15,6 +15,20 @@ router.get("/", (request, response) => {
     response.json(pokeDB)
 })
 
+router.get("/:pokeId", (request, response) => {
+    let pokeId = request.params.pokeId
+    let pokeData = catchPokemon(pokeId)
+    
+    if(pokeData) {
+        response.json(pokeData)
+    }
+    else {
+        response
+        .status(404)
+        .send("Not Found!")
+    }
+})
+
 // export router
 module.exports = router
 
